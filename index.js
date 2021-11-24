c59edd954a784ef4694d40319e00f7de5d9e963b
PrabeshOli <herokaje886@gmail.com>
2021-11-24 10:26:38 +0545

    Simple Calculator

---
+++ index.js
@@ -0,0 +1,42 @@
+const prompt = require("prompt-sync")();
+
+let result;
+
+const operator = prompt('Enter operator (+, -, *,/,% or ** ): ');
+
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+switch(operator) {
+    case '+':
+         result = number1 + number2;
+        console.log(`${number1} + ${number2} = ${result}`);
+        break;
+
+    case '-':
+         result = number1 - number2;
+        console.log(`${number1} - ${number2} = ${result}`);
+        break;
+
+    case '*':
+         result = number1 * number2;
+        console.log(`${number1} * ${number2} = ${result}`);
+        break;
+
+    case '/':
+         result = number1 / number2;
+        console.log(`${number1} / ${number2} = ${result}`);
+        break;
+    case '%':
+        result= number1 % number2;
+        console.log(`${number1} % ${number2}= ${result}`);
+        break;
+    case '**':
+            result= number1 ** number2;
+            console.log(`${number1} ** ${number2}= ${result}`);
+            break;
+
+    default:
+        console.log('Invalid operator');
+        break;
+}
